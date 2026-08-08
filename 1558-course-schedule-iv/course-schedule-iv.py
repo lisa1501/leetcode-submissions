f4f890bb66747fd4672f2ed02842b50eab0bf288
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-08 10:55:01 -0700

    Time: 3 ms (89.57%) | Memory: 19.6 MB (91.86%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,6 +1,8 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-
+        
+        # time : Time: O(V*V+ E + Q) Space: O(V*V + E) V:num of courses, E:num of prerequisites, Q:num of queries
+        
         pre_to_crs = defaultdict(list)
         count_pres = [0] * numCourses
 
@@ -18,13 +20,12 @@ class Solution:
         while q:
             pre_crs = q.popleft()
             
-            for nxt_crs in pre_to_crs[pre_crs]:
+            for nxt_crs in pre_to_crs[pre_crs]: #O(V)
                 # course is a direct prerequisite of nxt
                 prereq[nxt_crs].add(pre_crs)
-
                 # All prerequisites of course
                 # are also prerequisites of nxt
-                prereq[nxt_crs].update(prereq[pre_crs])
+                prereq[nxt_crs].update(prereq[pre_crs]) #O(V)
 
                 count_pres[nxt_crs] -= 1
 
