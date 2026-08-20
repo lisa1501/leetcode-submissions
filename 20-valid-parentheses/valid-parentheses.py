7d76ce28c013290ced47484e35f968ef22be991a
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-20 12:12:51 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (62.89%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -6,10 +6,12 @@ class Solution:
         for p in s:
             if p not in collect:
                 stack.append(p)
-
             else:
-                if stack and stack[-1] == collect[p]:
-                    stack.pop()
+                if stack:
+                    if stack[-1] == collect[p]:
+                        stack.pop()
+                    else:
+                        return False
                 else:
                     return False
         return len(stack) == 0
