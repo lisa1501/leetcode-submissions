1179fe75e2ddb1754ae05a6a207675127bcf3463
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-21 07:32:28 -0700

    Time: 0 ms (100.00%) | Memory: 20.1 MB (57.47%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -6,27 +6,15 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        #  dfs, 
-        # path sum  =0
-        # start with root, i
-        # increase path sum by current node.val
-        # parent node return path sum + parent ndoe val then transfer this to left and rieght child
-        # when we met path sum == targetsum => ture
-        # visiting all node, paths there is no path sum == target, return false
-        # time :O(n),O(n)
-        def dfs(node, path_sum):
-            if not node:
+        def dfs(node, num):
+            if not node :
                 return False
 
-            path_sum += node.val
-
+            num += node.val
             if not node.left and not node.right:
-                return path_sum == targetSum
+                return num == targetSum
 
-            left = dfs(node.left, path_sum) 
-            right = dfs(node.right, path_sum)
-        
-            return left or right
+            return dfs(node.left, num) or dfs(node.right, num)
 
         return dfs(root, 0)
         
