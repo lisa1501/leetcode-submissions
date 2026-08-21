aefe3dd21b2d56da5eb5c414ed3ccc6e29c93e7f
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-20 19:21:50 -0700

    Time: 0 ms (100.00%) | Memory: 20.1 MB (98.72%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,32 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pre_to_course = defaultdict(list)
+        indegree = [0] * numCourses
+
+        for course, pre_course in prerequisites:
+            pre_to_course[pre_course].append(course)
+            indegree[course] += 1
+            
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+
+        completed = 0
+
+        while q:
+            pre_crs = q.popleft()
+            completed += 1
+
+            for nxt_crs in pre_to_course[pre_crs]:
+                indegree[nxt_crs] -= 1
+
+                if indegree[nxt_crs] == 0:
+                    q.append(nxt_crs)
+
+        return completed == numCourses
+
+
+
+
+        
