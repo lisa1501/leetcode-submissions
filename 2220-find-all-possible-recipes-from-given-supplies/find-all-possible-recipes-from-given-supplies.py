ee15a0bc7dc9072f7e144edcfe334d8f31994b4c
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-21 08:17:59 -0700

    Time: 60 ms (75.20%) | Memory: 21.5 MB (88.22%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.py
@@ -1,44 +1,38 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        # Topological Sort graph modeling 
-        # build two grpah, 
-        # 1st: one recipe to len ingredient, 
-        # 2nd: one ingredient to list of recipe
-        # store supplies in a queue
-        # empty list all the recipes that we can create
-        # popleft queue, supplie => ingredient
-        # loop thru all recipe of supplie in 2nd grpah[ingredient], 
-        # decrease len ingredients in 1st grpah[recipe],
-        # after decreasing, if 1st grpah[recipe] ==0, queue append recipe, list append recipe
-        # return the created list
-        # time:O(R + I + S) space:O(R + I + S) R: total recipes, I: total ingredients, S: total supplies
-        graph = defaultdict(list)
-        indegree = defaultdict(int)
+        ingredient_to_recipes = defaultdict(list)
+        recipe_to_ingredients_list = defaultdict(int)
 
-        for recipe, ing_list in zip(recipes, ingredients):
+        for recipe, ingredient in zip(recipes, ingredients):
+            recipe_to_ingredients_list[recipe] = len(ingredient)
+            for ingre in ingredient:
+                ingredient_to_recipes[ingre].append(recipe)
 
-            indegree[recipe] = len(ing_list)
-
-            for ing in ing_list:
-                graph[ing].append(recipe)
-
-        queue = deque(supplies)
-
+        q = deque(supplies)
         result = []
 
-        while queue:
+        while q:
+            for _ in range(len(q)):
+                ingre = q.popleft()
 
-            item = queue.popleft()
+                for recipe in ingredient_to_recipes[ingre]:
+                    recipe_to_ingredients_list[recipe] -= 1
 
-            for recipe in graph[item]:
-
-                indegree[recipe] -= 1
-
-                if indegree[recipe] == 0:
-
-                    result.append(recipe)
-
-                    queue.append(recipe)
+                    if recipe_to_ingredients_list[recipe] == 0:
+                        q.append(recipe)
+                        result.append(recipe)
 
         return result
+
+        
+
+
+
+
+
+                
+
+        
+
+
         
