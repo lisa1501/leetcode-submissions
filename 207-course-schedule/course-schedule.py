bccb8d2abf3caed8186ba2c163cfb89c000dec63
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-20 19:30:54 -0700

    Time: 4 ms (64.40%) | Memory: 20.4 MB (62.04%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -6,7 +6,7 @@ class Solution:
         for course, pre_course in prerequisites:
             pre_to_course[pre_course].append(course)
             indegree[course] += 1
-            
+
         q = deque()
         for i in range(numCourses):
             if indegree[i] == 0:
@@ -15,16 +15,47 @@ class Solution:
         completed = 0
 
         while q:
-            pre_crs = q.popleft()
-            completed += 1
+            for _ in range(len(q)):
+                pre_crs = q.popleft()
+                completed += 1
 
-            for nxt_crs in pre_to_course[pre_crs]:
-                indegree[nxt_crs] -= 1
+                for nxt_crs in pre_to_course[pre_crs]:
+                    indegree[nxt_crs] -= 1
 
-                if indegree[nxt_crs] == 0:
-                    q.append(nxt_crs)
+                    if indegree[nxt_crs] == 0:
+                        q.append(nxt_crs)
 
         return completed == numCourses
+
+# Parallel Courses — LC 1136
+        # pre_to_course = defaultdict(list)
+        # indegree = [0] * numCourses
+
+        # for course, pre_course in prerequisites:
+        #     pre_to_course[pre_course].append(course)
+        #     indegree[course] += 1
+            
+        # q = deque()
+        # for i in range(numCourses):
+        #     if indegree[i] == 0:
+        #         q.append(i)
+
+        # completed = 0
+        # smesters = 0
+
+        # while q:
+        #     smesters += 1
+        #     for _ in range(len(q))
+        #       pre_crs = q.popleft()
+        #       completed += 1
+
+        #       for nxt_crs in pre_to_course[pre_crs]:
+        #           indegree[nxt_crs] -= 1
+
+        #           if indegree[nxt_crs] == 0:
+        #               q.append(nxt_crs)
+
+        # return semesters if completed == numCourses else -1
 
 
 
