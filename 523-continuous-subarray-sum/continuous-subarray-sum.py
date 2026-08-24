c591305a2858c38624efb5ba6501c224fd5573d7
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-24 13:19:17 -0700

    Time: 51 ms (69.27%) | Memory: 39.5 MB (78.91%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        ans = 0
-        prefix = 0
-        seen = {0 : -1} 
+
+        seen = {0:-1}
+        prefix_sum = 0
 
         for i in range(len(nums)):
-            prefix += nums[i]
-            reminder = prefix % k
+            prefix_sum += nums[i]
+            reminder = prefix_sum % k
 
             if reminder in seen:
                 if i - seen[reminder] >= 2:
@@ -14,5 +14,7 @@ class Solution:
             else:
                 seen[reminder] = i
         return False
+
+        
         
         
