7a2e93b5ed3a24d2680ae8ebfc54b401ed25dfa6
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-24 10:52:42 -0700

    Time: 3 ms (60.77%) | Memory: 19.4 MB (99.42%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.py
@@ -0,0 +1,34 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = [i for i in range(n+1)]
+        self.size = [1] * (n+1)
+
+    def find(self, node):
+        if self.parent[node] != node:
+            self.parent[node] = self.find(self.parent[node])
+        return self.parent[node]
+
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+
+        if pu == pv:
+            return False
+
+        if self.size[pu] < self.size[pv]:
+            pu, pv = pv, pu
+
+        self.size[pu] += self.size[pv]
+        self.parent[pv] = pu
+        return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+
+        dsu = DSU(n)
+
+        for u,v in edges:
+            if not dsu.union(u,v):
+                return [u,v]
+        
