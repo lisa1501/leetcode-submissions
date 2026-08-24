46df2e190386e5f77cb2b447f6dddb079976de84
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-24 14:21:12 -0700

    Time: 75 ms (83.68%) | Memory: 40.3 MB (17.60%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
@@ -15,12 +15,20 @@ class Solution:
             num = nums[i]
             prefix = (prefix + num) % p
             needed = (prefix - target) % p
+            # i=0, num=3, prefix=3, needed=5, seen={0:-1, 3:0}
+            # i=1, num=1, prefix=4, needed=0, 
+                # length = 1 - (-1)=2, ans = 2, seen={0:-1, 3:0, 4:1}
+            # i=2, num=4, prefix=2, needed=4, 
+                # length = 2 - (-1) = 1, ans = 1, seen={0:-1, 3:0, 4:1, 2:2}
+            # i=3, num=2, prefix=4, needed=0 
+                # length = 3 - (-1) = 4, ans = 1, 
 
             if needed in seen:
                 lenght = i - seen[needed]
                 ans = min(ans, lenght)
 
-            seen[prefix] = i
+            seen[prefix] = i 
+
 
         if ans == len(nums):
             return -1
