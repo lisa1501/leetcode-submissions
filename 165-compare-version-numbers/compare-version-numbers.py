76ef8ae0c628e98d50f4d83e0fe566c7a12031ee
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-25 12:54:21 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (29.11%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.py
+++ 165-compare-version-numbers/compare-version-numbers.py
@@ -1,9 +1,10 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        v1, v2 = list(map(int, version1.split('.'))), list(map(int, version2.split('.')))
-        print(v1, v2)  
-        v3 = [ int(s) for s in version1.split('.')]
-        v4 = [ int(s) for s in version2.split('.')]
+ 
+        v1 = [ int(s) for s in version1.split('.')]
+        v2 = [ int(s) for s in version2.split('.')]
+        print(v1, v2)
+
         for rev1, rev2 in zip_longest(v1, v2, fillvalue=0):
             if rev1 == rev2:
                 continue
