db8a291cd8a884fd6c01e7f5620081a2b27c0754
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-25 11:58:37 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (19.58%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -3,7 +3,7 @@ class Solution:
         write = 0
         for read in range(len(nums)):
             if nums[read] != val:
-                nums[write] = nums[read]
+                nums[write], nums[read] = nums[read], nums[write]
                 write += 1
         return write
         
