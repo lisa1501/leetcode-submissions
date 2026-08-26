aeeeb47304afb51124aaedc24089ce0192db9d85
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-26 10:22:14 -0700

    Time: 56 ms (14.92%) | Memory: 19.8 MB (49.96%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -7,29 +7,30 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        parents = {}
+        parent ={}
         def dfs(node, par):
             if not node:
                 return 
-            parents[node] = par
+
+            parent[node] = par
             dfs(node.left, node)
             dfs(node.right, node)
+
         dfs(root, None)
 
-        visit = set()
-        visit.add(target)
+        visited = set()
+        visited.add(target)
+        q = deque([(target, 0)])
         res = []
-        q = deque([(target, 0)]) # node, distance from node to target
+        
         while q:
             node, distance = q.popleft()
+
             if distance == k:
                 res.append(node.val)
 
-            for nei in (node.left, node.right, parents[node]):
-                if nei and nei not in visit:
-                    visit.add(nei)
+            for nei in (node.left, node.right, parent[node]):
+                if nei and nei not in visited:
+                    visited.add(nei)
                     q.append((nei, distance+1))
-                        
         return res
-
-
