61c72c00c5f894983e1c4bb187ae33f4280131ef
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-30 12:43:14 -0700

    Time: 28 ms (34.85%) | Memory: 22.9 MB (69.36%) - LeetSync

--- 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
+++ 1116-maximum-level-sum-of-a-binary-tree/maximum-level-sum-of-a-binary-tree.py
@@ -6,29 +6,27 @@
 #         self.right = right
 class Solution:
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        res = []
+        max_sum = float('-inf')
+        max_level = None
+        q = deque([(root, 1)])
 
-        def dfs(node, depth):
-            if not node:
-                return 
+        while q:
+            level_sum = 0
+            for _ in range(len(q)):
+                node, level = q.popleft()
+                level_sum += node.val
+                if node.left:
+                    q.append((node.left, level+1))
+                if node.right:
+                    q.append((node.right, level+1))
 
-            if depth == len(res):
-                res.append([])
-            res[depth].append(node.val)
+            if level_sum > max_sum:
+                max_sum = level_sum
+                max_level = level
 
-            left = dfs(node.left, depth + 1)
-            right = dfs(node.right, depth + 1)
+        return max_level
 
-        dfs(root, 0)
+                
 
-        max_val = float('-inf')
-        max_val_level = None
-
-        for i in range(len(res)):
-            if sum(res[i]) > max_val:
-                max_val = sum(res[i]) 
-                max_val_level = i 
-
-        return max_val_level + 1
 
         
