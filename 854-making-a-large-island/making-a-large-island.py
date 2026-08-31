cf672a62707f01f41de7449057f32fa94b53c4c5
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-08-30 17:41:08 -0700

    Time: 627 ms (91.01%) | Memory: 83.6 MB (7.69%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -3,53 +3,53 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         dirs = [(0,1), (0,-1), (1,0), (-1,0)]
-        island_id = 2
-        area = {}
+        visited = set()
+        ans = 0
+        islandId_to_size = {}
+        islandId = 2
 
-        def dfs(r, c, idx):
-            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != 1:
+        def dfs(r, c, num):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0 or (r,c) in visited:
                 return 0
 
-            grid[r][c] = idx
+            visited.add((r,c))
+            grid[r][c] = num
             size = 1
 
             for dr, dc in dirs:
-                nr = dr + r
-                nc = dc + c
-                size += dfs(nr, nc,idx)
+                nr = r + dr
+                nc = c + dc
+                size += dfs(nr, nc, num)
 
             return size
 
-        
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 1:
-                    area[island_id] = dfs(r,c,island_id)
-                    island_id += 1
-                        
+                if grid[r][c] == 1 and (r,c) not in visited:
+                    islandId_to_size[islandId] = dfs(r, c, islandId)
+                    islandId += 1
 
-        if len(area) == 0:
+        if len(islandId_to_size) == 0:
             return 1
-        ans = max(area.values())       
+        ans = max(islandId_to_size.values())
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 0:
                     total = 1
                     seen = set()
+
                     for dr, dc in dirs:
-                        nr = dr + r
-                        nc = dc + c
-                        if 0 <= nr < rows and 0 <= nc < cols:
+                        nr = r + dr
+                        nc = c + dc
+
+                        if 0<=nr<rows and 0<=nc<cols:
                             nei_id = grid[nr][nc]
 
                             if nei_id not in seen:
                                 seen.add(nei_id)
-                                total += area.get(nei_id, 0)
+                                total += islandId_to_size.get(nei_id,0)
 
                     ans = max(ans, total)
+
         return ans
-
-                        
-
-        
