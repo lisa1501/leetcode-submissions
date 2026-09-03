e66ea5d5b7affc62ea8c17d557fd9f79e1b5049d
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-09-03 11:23:44 -0700

    Time: 31 ms (20.43%) | Memory: 19.3 MB (53.86%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.py
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -14,28 +14,27 @@ class Solution:
 
             return row, col
 
-
         visited = set()
         visited.add(1)
         q = deque([(1,0)])
 
         while q:
-            state, steps = q.popleft()
+            for _ in range(len(q)):
+                state, steps = q.popleft()
 
-            if state == n * n:
-                return steps
+                if state == n * n:
+                    return steps
 
-            for nxt in range(state+1, min(state+6, n*n) +1):
-                r, c = get_position(nxt)
+                for nxt in range(state+1, min(state+6, n*n) +1):
+                    r, c = get_position(nxt)
 
-                dst = nxt
+                    dst = nxt
 
-                if board[r][c] != -1:
-                    dst = board[r][c]
+                    if board[r][c] != -1:
+                        dst = board[r][c]
 
-
-                if dst not in visited:
-                    visited.add(dst)
-                    q.append((dst, steps+1))
+                    if dst not in visited:
+                        visited.add(dst)
+                        q.append((dst, steps+1))
         return -1
         
