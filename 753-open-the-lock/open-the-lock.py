ed67a99787b5dc6c3a3bea4f676741794e29a0a1
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-09-03 10:40:58 -0700

    Time: 362 ms (43.13%) | Memory: 20.5 MB (53.11%) - LeetSync

--- 753-open-the-lock/open-the-lock.py
+++ 753-open-the-lock/open-the-lock.py
@@ -1,29 +1,32 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         deads = set(deadends)
+
         if "0000" in deads:
             return -1
 
-        q = deque([("0000", 0)])
         visited = set()
         visited.add("0000")
+        q = deque([("0000", 0)])
 
         while q:
-            state, steps = q.popleft()
-            if state == target:
-                return steps
+            for _ in range(len(q)):
+                state, steps = q.popleft()
 
-            for i in range(4):
-                cur_num = int(state[i])
+                if state == target:
+                    return steps
 
-                for move in [-1,1]:
-                    new_num = (cur_num + move) % 10
+                for i in range(len(state)):
+                    cur_num = int(state[i])
 
-                    new_state = state[:i] + str(new_num) + state[i+1:]
+                    for move in [-1, 1]:
+                        new_num = (cur_num + move) % 10
 
-                    if new_state not in deads and new_state not in visited:
-                        q.append((new_state, steps + 1))
-                        visited.add(new_state)
+                        new_state = state[:i] + str(new_num) + state[i+1:]
+
+                        if new_state not in deads and new_state not in visited:
+                            q.append((new_state, steps + 1))
+                            visited.add(new_state)
         return -1
 
 
