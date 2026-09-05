17dd690a004a57d730b9bf4cf4b699ac79ac1707
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-09-05 14:36:37 -0700

    Time: 79 ms (65.29%) | Memory: 40.1 MB (65.77%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
@@ -1,36 +1,54 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        total = sum(nums)
-        target = total % p
-        
+        # nums = [3,1,4,2], p = 6
+        # prefix sum
+        # return smallest =>  min val , min(len(nums), subarr len)
+        # prefix sum, hm store prefxi sum: cur num idx, lenght  =0 
+        # {0:1, 3:0, 4:1}
+        # calculate sum of nums, module p, target, 10%6 =4
+        # if reminder is 0 return 0
+        # for loop on nums, 
+        #  increase prefix sum by cur num, 
+        # prefix sum % p, 
+        # (prefix sum - target) % p in hm or 
+        # if in hm :
+        # cur len is cur idx - what we see in hm , the previout idx num
+        # updated smallest min of cur len  and len of nums
+        # hm[prefixsum] = cur idx
+        # updated smallest is len of nums reutrn -1
+        # return updated smallest
+
+        # time: O(n), Space:O(n)
+        # [3,1,4,2], p = 6
+
+        total = sum(nums) #10
+        target = total % p #4
         if target == 0:
             return 0
 
-        prefix = 0
-        seen = {0:-1}
+        prefix_sum = 0
+        seen = {0: -1}
         ans = len(nums)
         length = 0
 
-        for i in range(len(nums)):
-            num = nums[i]
-            prefix = (prefix + num) % p
-            needed = (prefix - target) % p
-            # i=0, num=3, prefix=3, needed=5, seen={0:-1, 3:0}
-            # i=1, num=1, prefix=4, needed=0, 
-                # length = 1 - (-1)=2, ans = 2, seen={0:-1, 3:0, 4:1}
-            # i=2, num=4, prefix=2, needed=4, 
-                # length = 2 - (-1) = 1, ans = 1, seen={0:-1, 3:0, 4:1, 2:2}
-            # i=3, num=2, prefix=4, needed=0 
-                # length = 3 - (-1) = 4, ans = 1, 
+        for i, num in enumerate(nums):
+            prefix_sum = (prefix_sum + num) % p
+            needed = (prefix_sum - target) % p
 
             if needed in seen:
-                lenght = i - seen[needed]
-                ans = min(ans, lenght)
+                length = i - seen[needed]
+                ans = min(ans, length)
 
-            seen[prefix] = i 
-
+            seen[prefix_sum] = i
 
         if ans == len(nums):
-            return -1
+            return -1 
+    
         return ans
+
+
+
+
+
+
         
