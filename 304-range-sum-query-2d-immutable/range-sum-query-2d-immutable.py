467e5bc625b70a00437fd52c49e4463e67998206
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-09-22 17:49:24 -0700

    Time: 75 ms (99.30%) | Memory: 34.2 MB (17.42%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -2,7 +2,7 @@ class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
         rows = len(matrix)
-        cols =len(matrix[0])
+        cols = len(matrix[0])
 
         self.prefix_matrix = [[0] * (cols + 1) for _ in range(rows + 1)]
 
@@ -13,13 +13,13 @@ class NumMatrix:
                 above = self.prefix_matrix[r][c + 1]
                 self.prefix_matrix [r + 1][c + 1] = prefix + above
 
-
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
 
         bottomRight = self.prefix_matrix[row2+1][col2+1]
-        topRight = self.prefix_matrix [row1][col2+1]
-        bottomleft = self.prefix_matrix [row2+1][col1]
-        topLeft = self.prefix_matrix [row1][col1]
+        bottomleft = self.prefix_matrix[row2+1][col1]
+
+        topRight = self.prefix_matrix[row1][col2+1]
+        topLeft = self.prefix_matrix[row1][col1]
 
         return bottomRight - topRight - bottomleft + topLeft
 
