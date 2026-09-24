cee9b396203d1bc2bc22e2cb653ac84ac8f34e37
Lisa  S <48338585+lisa1501@users.noreply.github.com>
2026-09-24 13:25:55 -0700

    Time: 61 ms (23.77%) | Memory: 57 MB (60.81%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.py
+++ 1731-even-odd-tree/even-odd-tree.py
@@ -5,48 +5,49 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
-        # time:O(n), space:O(n)
-        res = []
-        if not root:
-            return res
+    def isEvenOddTree(self, root: TreeNode | None) -> bool:
         q = deque([root])
-
+        res = []
         while q:
             level = []
-            for i in range(len(q)):
+            for _ in range(len(q)):
                 node = q.popleft()
-                if node:
-                    level.append(node.val)
+                level.append(node.val)
 
-                    if node.left:
-                        q.append(node.left)
-                    if node.right:
-                        q.append(node.right)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
             res.append(level)
 
         for i in range(len(res)):
             if i % 2 == 0:
-                prev = float('-inf')
-                for ele in res[i]:
-                    if ele % 2 == 0:
+                prev_val = float('-inf')
+                for num in res[i]:
+                    if num % 2 == 0:
                         return False
-                    if ele <= prev:
-                        return False
-                    prev = ele
+
+                    if num > prev_val:
+                        prev_val = max(prev_val, num)
+                    else:
+                        return False    
             else:
-                prev = float('inf')
-                for ele in res[i]:
-                    if ele % 2 == 1:
+                prev_val = float('inf')
+                for num in res[i]:
+
+                    if num % 2 == 1:
                         return False
-                    if ele >= prev:
+
+                    if num < prev_val:
+                        prev_val = min(prev_val, num)         
+                    else:
                         return False
-                    prev = ele
+                        
         return True
-                    
-            
 
 
 
+
+                
 
         
